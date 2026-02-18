fa6730a1b3cb73a2962903da0c252dd3ac15fc83
Tutardo23 <matiastorres2783@gmail.com>
2026-02-17 21:39:34 -0300

    p

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,7 +3,7 @@ import createImageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
   projectId: "wrubiu1d", // Tu ID real
-  dataset: "production", // Asumimos que es production
+  dataset: "blog", // Asumimos que es production
   apiVersion: "2024-01-01",
   useCdn: false, // ESTO ES CLAVE: False para que no guarde memoria vieja
 });
